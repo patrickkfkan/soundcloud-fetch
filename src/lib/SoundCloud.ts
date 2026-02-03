23139ba731de7101f4b79dbda98a22031466bcb7
patrickkfkan <patrickkfkan@gmail.com>
2026-02-03 18:17:02 +0800

    Update user-agent

--- src/lib/SoundCloud.ts
+++ src/lib/SoundCloud.ts
@@ -87,7 +87,7 @@ export default class SoundCloud {
       Origin: 'https://soundcloud.com',
       Referer: 'https://soundcloud.com/',
       'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.3'
+          'Mozilla/5.0 (X11; Linux x86_64; rv:138.0) Gecko/20100101 Firefox/138.0'
     });
 
     if (method === 'POST') {
